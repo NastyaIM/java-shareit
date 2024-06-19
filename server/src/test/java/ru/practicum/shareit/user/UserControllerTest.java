e0ccafdf2d9741aa89a81bf373a92acbfd3ddcdf
Nastya <nimalyshevann@gmail.com>
2024-06-19 11:19:26 +0300

    fix: комменты

--- server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -116,15 +116,15 @@ class UserControllerTest {
         verify(userService).update(id, userToUpdate);
     }
 
-//    @SneakyThrows
-//    @Test
-//    void deleteUser() {
-//
-//        mockMvc.perform(delete(PathConstants.USERS + PathConstants.BY_ID, id))
-//                .andExpect(status().isOk());
-//
-//        verify(userService).delete(id);
-//    }
+    @SneakyThrows
+    @Test
+    void deleteUser() {
+
+        mockMvc.perform(delete(PathConstants.USERS + PathConstants.BY_ID, id))
+                .andExpect(status().isOk());
+
+        verify(userService).delete(id);
+    }
 
     @SneakyThrows
     @Test
@@ -141,17 +141,4 @@ class UserControllerTest {
 
         verify(userService).save(userDto);
     }
-
-//    @SneakyThrows
-//    @Test
-//    void saveWhenUserIsNotValid() {
-//        userDto.setEmail("Incorrect Email");
-//
-//        mockMvc.perform(post(PathConstants.USERS)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(userDto)))
-//                .andExpect(status().isBadRequest());
-//
-//        verify(userService, never()).save(userDto);
-//    }
 }
